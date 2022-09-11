9cba68cbd6d3a2c1ede9c5abcd4cf515fedcaf72
opitt <23746293+opitt@users.noreply.github.com>
2022-09-11 12:43:34 +0200

    Make logic and solution more readable

--- 0/0.py
+++ 0/0.py
@@ -1,4 +1,5 @@
-base = "http://www.pythonchallenge.com/pc/def/0.html"
-url,_ = base.split("0")
+url_0 = "http://www.pythonchallenge.com/pc/def/0.html"
 sol = f"{2**38}"
-print(f"{url}{sol}.html")
+url_1 = url_0.replace("0", sol)
+
+print(f"\nOpen now puzzle 1: {url_1}\n")
