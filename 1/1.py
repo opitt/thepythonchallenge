f994a5f894893bb8c0f4d395198f17ba8a77e21f
opitt <23746293+opitt@users.noreply.github.com>
2022-09-11 12:40:36 +0200

    Solution for puzzle 1

---
+++ 1/1.py
@@ -0,0 +1,11 @@
+table = "".maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+
+text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+text = text.translate(table)
+
+print(text.replace(". ", ".\n"))
+
+url_1 = "http://www.pythonchallenge.com/pc/def/map.html"
+url_2 = url_1.replace("map", "map".translate(table))
+
+print(f"\n{url_2}\n")
